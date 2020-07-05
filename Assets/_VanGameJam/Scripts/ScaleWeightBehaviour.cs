4fe84519469a2a1a8bfe20a423d3b901b3826599
Martin Tsang <m4rtin.t@gmail.com>
2020-07-04 21:02:55 -0700

    Added in Score scorling

--- Assets/_VanGameJam/Scripts/ScaleWeightBehaviour.cs
+++ Assets/_VanGameJam/Scripts/ScaleWeightBehaviour.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 public class ScaleWeightBehaviour : MonoBehaviour
 {
+    //Animation
+    private const float RollingSpeed = 0.05f;
+    private const float GoUpBy = 50;
     private const string Suffix = " g";
-    private float _weight = 0;
-    private TMP_Text _weightText;
 
-    public float Weight => _weight;
+    private float _rollingDuration = 0;
+    private float _weight = 0;
+    private float _destinationWeight = 0;
+    private TMP_Text _weightText;
     
+
     private TMP_Text WeightText
     {
         get
@@ -30,23 +34,40 @@ public class ScaleWeightBehaviour : MonoBehaviour
 
     public void AddWeight(float weight)
     {
-        _weight += weight;
-        SetWeightText();
-    }
-
-    private void SetWeight(float weight)
-    {
-        _weight = weight;
-        SetWeightText();
+        _destinationWeight += weight;
     }
 
     public void Reset()
     {
-        SetWeight(0);
+        _destinationWeight = 0;
+        _weight = 0;
+        SetWeightText();
+    }
+
+    private void Update()
+    {
+        if (_destinationWeight > _weight)
+        {
+            _rollingDuration -= Time.deltaTime;
+            if (_rollingDuration < 0)
+            {
+                _rollingDuration = RollingSpeed;
+                if (_destinationWeight - _weight > GoUpBy)
+                {
+                    _weight += GoUpBy;
+                }
+                else
+                {
+                    _weight = _destinationWeight;
+                }
+            
+                SetWeightText();
+            }
+        }
     }
 
     private void SetWeightText()
     {
-        WeightText.text = $"{Weight}{Suffix}";
+        WeightText.text = $"{_weight}{Suffix}";
     }
 }
